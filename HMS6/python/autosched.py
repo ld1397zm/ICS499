695b69d33b13ce8c064c6f56cb1045c774800c91
ld1397zm <46947914+ld1397zm@users.noreply.github.com>
2019-11-25 09:46:02 -0600

    AutoScheduler save to DB
    Schedules are now saved to the DB in a new autoschedule table with column headers "Floor", "Room", and "Employee".

--- HMS6/python/autosched.py
+++ HMS6/python/autosched.py
@@ -12,6 +12,8 @@ from random import shuffle
 "In case DB commands change, they're all listed here."
 SCHED_CMD = "select * from schedule order by Room asc;"
 EMPL_CMD = "select distinct Employee from schedule where Employee is not null ;"
+CREATE_AUTOSCHEDULE = "create table autoschedule (Floor INT, Room INT, Employee VARCHAR(64));"
+DELETE_AUTOSCHEDULE = "drop table autoschedule;"
 NEED_CLEAN = "Un-Cleaned"
 ROOM_AVAIL = "Make Up My Room"
 
@@ -23,43 +25,6 @@ class Room:
         self.floorNumber = floorNumber
         self.roomStatus = roomStatus
         self.roomAlert = roomAlert
-
-
-class User:
-    def __init__(self, fname, lname):
-        self.fname = fname
-        self.lname = lname
-
-
-class Manager(User):
-    def __init__(self, fname, lname):
-        User(fname, lname).__init__(fname, lname)
-
-    def create_account(self):
-        """TODO"""
-
-    def delete_account(self):
-        """TODO"""
-
-    def modify_account(self):
-        """TODO"""
-
-    def add_room(self):
-        """TODO"""
-
-    def remove_room(self):
-        """TODO"""
-
-    def modify_room(self):
-        """TODO"""
-
-
-class Employee(User):
-    def __init__(self, fname, lname):
-        User(fname, lname).__init__(fname, lname)
-
-    def export_schedule(self):
-        """TODO"""
 
 
 class DBInterface:
@@ -86,17 +51,41 @@ class DBInterface:
         return ret
 
     def get_employees(self):
+        """Get a distinct list of employees"""
         self.sql.execute(EMPL_CMD)
 
         ret = []
+        """Format employee list"""
         for row in self.sql.fetchall():
             ret.append(row[0])
 
         return ret
 
-    def save_sched_to_db(self, sched):
-        """TODO"""
+    def reset_autosched_table(self):
+        tables = []
+        """Get and format a list of tables"""
+        self.sql.execute("show tables")
+        for i in self.sql:
+            tables.append(i[0])
 
+        """Reset autoschedule table to empty state"""
+        if 'autoschedule' in tables:
+            self.sql.execute(DELETE_AUTOSCHEDULE)
+        self.sql.execute(CREATE_AUTOSCHEDULE)
+
+    def save_sched_to_db(self, sched, names):
+        self.reset_autosched_table()
+
+        """commit = []"""
+
+        """Format a sql query to add row, then execute"""
+        for i in range(len(names)):
+            for j in sched[i]:
+                """commit.append([j.floorNumber, j.roomNumber, names[i]])"""
+                self.sql.execute("insert into autoschedule values (" + j.floorNumber + ", " + j.roomNumber + ", '" + names[i] + "');")
+
+        """Ensure changes are reflected in DB"""
+        self.cnx.commit()
 
 class AutoScheduler:
     def __init__(self):
@@ -141,3 +130,6 @@ class AutoScheduler:
         shuffle(ret)
 
         return ret
+
+    def save_sched_to_db(self):
+        self.db.save_sched_to_db(self.assign_rooms(), self.empl)
